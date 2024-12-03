11b9ad98f615d3d800a175815b7cd6290afa3ee4
Nitin Gupta <niting1209@gmail.com>
2024-12-03 14:40:37 -0500

    Checking deployement

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/medium-network-viz/',
+  base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
   },
   server: {
